9e9ca9045af3d8fcdd9fe4397e4d026ce7a9bd0d
Andy Yang <andytheyang@gmail.com>
2017-03-14 11:11:14 -0700

    added scripts to change dpi

---
+++ extern_monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface text-scaling-factor 1
+
+exit 0;
---
+++ reg_monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+
+exit 0;
